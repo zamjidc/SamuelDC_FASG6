53fec443da4252007eac65fe8c95ddea33806efa
zamjidc <132146429+zamjidc@users.noreply.github.com>
2023-06-16 11:38:47 +0800

    Add files via upload

---
+++ recipe_service.dart
@@ -0,0 +1,44 @@
+import 'package:chopper/chopper.dart';
+import 'recipe_model.dart';
+import 'model_response.dart';
+import 'model_converter.dart';
+import 'service_interface.dart';
+
+part 'recipe_service.chopper.dart';
+
+const String apiKey = '7e42d58aebad58d29d0f23d6029f8fb8';
+const String apiId = 'facdff34';
+const String apiUrl = 'https://api.edamam.com';
+
+@ChopperApi()
+abstract class RecipeService extends ChopperService
+    implements ServiceInterface {
+  @override
+  @Get(path: 'search')
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+    @Query('q') String query,
+    @Query('from') int from,
+    @Query('to') int to,
+  );
+
+  static RecipeService create() {
+    final client = ChopperClient(
+      baseUrl: apiUrl,
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      converter: ModelConverter(),
+      errorConverter: const JsonConverter(),
+      services: [
+        _$RecipeService(),
+      ],
+    );
+    return _$RecipeService(client);
+  }
+}
+
+Request _addQuery(Request req) {
+  final params = Map<String, dynamic>.from(req.parameters);
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
+
+  return req.copyWith(parameters: params);
+}
